f2078966165a9a5ed70d1fd63ed6aedc928b105f
shwetashah <shah.shweta1912@gmail.com>
2022-05-03 17:05:54 +0100

    firstcommit

---
+++ src/homework.java
@@ -0,0 +1,13 @@
+public class homework {
+    public static void main(String args[]){
+        for (int a =2;a<=2; a++)
+        {
+            for(int b =1; b<=10; b++)
+            System.out.println(a + "*" +b+ "=" + a*b);
+        }
+
+
+    }
+
+
+}
